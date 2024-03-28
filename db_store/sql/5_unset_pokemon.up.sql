e8aba3d847e4ffef6f330413043f50dff0c4d68f
Chris Behrens <cbehrens@codestud.com>
2024-03-27 21:07:00 -0700

    Unset pokemon when disabling nest
    pokemon is not being unset when disabling nests due to overlap.
    This fixes it.

---
+++ db_store/sql/5_unset_pokemon.up.sql
@@ -0,0 +1,19 @@
+-- Fix procedure for overlap disablement
+-- I guess ST_Overlaps() is false if fully contained?
+-- So, just check we have Poly/MPoly before doing ST_Area().
+DROP PROCEDURE IF EXISTS fl_nest_filter_overlap;
+CREATE PROCEDURE fl_nest_filter_overlap (IN maximum_overlap double)
+BEGIN
+  DROP TEMPORARY TABLE IF EXISTS overlapNest;
+  CREATE TEMPORARY TABLE overlapNest AS (
+    SELECT b.nest_id
+    FROM nests a, nests b
+    WHERE a.active = 1 AND b.active = 1 AND
+        a.m2 > b.m2 AND
+        ST_Intersects(a.polygon, b.polygon) AND
+        ST_GeometryType(ST_Intersection(a.polygon, b.polygon)) IN ('Polygon', 'MultiPolygon') AND
+        (100 * ST_Area(ST_Intersection(a.polygon,b.polygon)) / ST_Area(b.polygon)) > maximum_overlap
+  );
+  UPDATE nests a, overlapNest b SET a.active=0,a.discarded='overlap',a.pokemon_id=NULL,a.pokemon_form=NULL,a.pokemon_avg=NULL,a.pokemon_count=NULL,a.pokemon_ratio=NULL,a.pokemon_avg=NULL WHERE a.nest_id=b.nest_id;
+  DROP TEMPORARY TABLE overlapNest;
+END
